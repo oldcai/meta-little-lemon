c919234e0f2bb130781d7e8a056f3dabb680584c
Xun Cai <hi.albert.tsai@gmail.com>
2024-01-20 15:58:53 +1100

    visual

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Main from './components/Main';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import Reservations from './pages/Reservations';
+import './App.css';
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reservations" element={<Reservations />} />
+      </Routes>
       <Footer />
     </>
   );
