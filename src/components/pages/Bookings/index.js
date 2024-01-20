485fc841290bf5fc79927ebf7756b217dc6511a0
Xun Cai <hi.albert.tsai@gmail.com>
2024-01-20 19:45:23 +1100

    improve design

--- src/components/pages/Bookings/index.js
+++ src/components/pages/Bookings/index.js
@@ -1,6 +1,5 @@
 import { useReducer } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import './index.css';
 import { fetchAPI, submitAPI } from '../../../utils/fakeAPI';
 import pages from '../../../utils/pages';
@@ -8,31 +7,31 @@ import BookingForm from './BookingForm';
 
 const updateTimes = (availableTimes, date) => {
   const response = fetchAPI(new Date(date));
-  return (response.length !== 0) ? response : availableTimes; 
+  return (response.length !== 0) ? response : availableTimes;
 };
 
-const initializeTimes = initialAvailableTimes => 
+const initializeTimes = initialAvailableTimes =>
   [...initialAvailableTimes, ...fetchAPI(new Date())];
 
 const Bookings = () => {
   const [
-    availableTimes, 
+    availableTimes,
     dispatchOnDateChange
   ] = useReducer(updateTimes, [], initializeTimes);
   const navigate = useNavigate();
 
-  const onSubmit = formData => {
+  const submitData = formData => {
     const response = submitAPI(formData);
     if (response) navigate(pages.get('confirmedBooking').path);
-  }; 
+  };
 
   return (
     <div className="bookings">
-      <h2>Table reservation</h2>
-      <BookingForm 
-        availableTimes={availableTimes} 
-        dispatchOnDateChange={dispatchOnDateChange} 
-        onSubmit={onSubmit} 
+      <h2>Book a Table</h2>
+      <BookingForm
+        availableTimes={availableTimes}
+        dispatchOnDateChange={dispatchOnDateChange}
+        submitData={submitData}
       />
     </div>
   );
--- src/components/pages/Bookings/index.test.js
+++ src/components/pages/Bookings/index.test.js
@@ -1,24 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-
 import Bookings from './';
 
 describe('Booking page', () => {
   const timeFormat = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
 
-  test('should have one or more available booking time options', () => {
+  test('should have one or more available booking time options', async () => {
     render(
       <MemoryRouter>
         <Bookings />
       </MemoryRouter>
     );
 
-    const timeOptions = screen.getAllByRole('option');
+    const timeOptions = await screen.findAllByTestId('booking-time-option');
 
     expect(timeOptions.length).toBeGreaterThanOrEqual(1);
+    timeOptions.forEach(timeOption => 
+      expect(timeOption.value).toMatch(timeFormat)
+    );
   });
 
-  test('should update available booking time options when changing booking date', () => {
+  test('should update available booking time options when changing booking date', async() => {
     render(
       <MemoryRouter>
         <Bookings />
@@ -27,8 +29,20 @@ describe('Booking page', () => {
 
     const bookingDate = '2023-04-01';
     const dateInput = screen.getByLabelText(/Date/);
+    const initialTimeOptions = await screen.findAllByTestId('booking-time-option');
     fireEvent.change(dateInput, { target: { value: bookingDate } });
     fireEvent.blur(dateInput);
+    const updatedTimeOptions = await screen.findAllByTestId('booking-time-option');
+
     expect(dateInput).toHaveValue(bookingDate);
+    initialTimeOptions.forEach(timeOption => 
+      expect(timeOption.value).toMatch(timeFormat)
+    );
+    updatedTimeOptions.forEach(timeOption => 
+      expect(timeOption.value).toMatch(timeFormat)
+    );
+    expect(initialTimeOptions.length).not.toBe(updatedTimeOptions.length);
   });
+
+  //test('', () => {});
 });
