a8be3b8ab3b48a3f79d13792b192f66946dc4b4d
Xun Cai <hi.albert.tsai@gmail.com>
2024-01-20 15:32:24 +1100

    set up layouts

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Reservation link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Reservation/i);
   expect(linkElement).toBeInTheDocument();
 });
